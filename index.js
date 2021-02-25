f41081c32e78bef997acac4dbd7e6ce6cb7d878b
Amelia Haba <ameliahaba347@gmail.com>
2021-02-25 00:52:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,27 @@ const nowServing = (katzDeliLine) => {
   return `Currently serving ${firstPersonInLine}.`
 }
 const currentLine = (katzDeliLine) => {
-  if (katzDeliLine.length === 0) return 'The line is currently empty.'
-  return `The line is currently: ${katzDeliLine.map((e, i) => `${i+1}. ${e}`).join(", ")}`
+  if (katzDeliLine.length === 0) return 'The line is currently empty.';
+
+  // Approach 0: for loop with int
+  let peopleWithNumbers = [];
+  for (let i = 0; i < katzDeliLine.length; i++) {
+    peopleWithNumbers.push(`${i+1}. ${katzDeliLine[i]}`);
+  }
+
+  // Approach 1: for loop with of
+  // let peopleWithNumbers = [];
+  // for (const person of katzDeliLine) {
+  //   peopleWithNumbers.push(`${peopleWithNumbers.length+1}. ${person}`);
+  // }
+
+  // Approach 2: map
+  // const peopleWithNumbers = katzDeliLine.map((element, number) => `${number+1}. ${element}`);
+
+  // Approach 3: forEach
+  // katzDeliLine.forEach((element, number) => {
+    // peopleWithNumbers.push(`${number+1}. ${element}`);
+  // });
+
+  return `The line is currently: ${peopleWithNumbers.join(", ")}`
 }
